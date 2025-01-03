e38b0aae768fc0ecdbf0efbf5485cb022c3edb8f
motakashi <takashi-going-sky@hotmail.co.jp>
2025-01-03 15:26:28 +0900

    簡単なAPIの作成

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+import express from "express";
+import type {Express, Request, Response } from "express";
+
+const app: Express = express();
+const PORT = 8080;
+
+app.get("/allTodos", (req: Request, res: Response) => {
+    return res.send("Todo");
+})
+
+app.listen(PORT, () => console.log("server is running"));
+console.log(`http://localhost:${PORT}/allTodos`);
