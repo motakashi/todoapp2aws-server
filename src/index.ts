0b518eccd9842791a9cf283288b36e2a6c3784fc
motakashi <takashi-going-sky@hotmail.co.jp>
2025-01-03 16:47:33 +0900

    Todo追加処理を追加

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,34 @@ import { PrismaClient } from '@prisma/client'
 const app: Express = express();
 const PORT = 8080;
 
+app.use(express.json());
 const prisma = new PrismaClient();
 
 app.get("/allTodos", async (req: Request, res: Response) => {
     const allTodos = await prisma.todo.findMany()
     return res.json(allTodos);
-})
+});
+
+app.post("/createTodo", async(req: Request, res: Response) => {
+    try {
+        console.log("aaaa")
+        console.log(req.body);
+        const {title, isCompleted} = req.body;
+        console.log(`title: ${title}, isCompeted: ${isCompleted}`);
+        const createTodo = await prisma.todo.create({
+            data: {
+              title,
+              isCompleted
+            },
+          })
+          return res.json(createTodo)
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "An error occurred while creating a todo." });
+    }
+
+});
 
 app.listen(PORT, () => console.log("server is running"));
 console.log(`http://localhost:${PORT}/allTodos`);
+console.log(`http://localhost:${PORT}/createTodo`);
