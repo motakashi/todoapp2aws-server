6f91cfd706376ae7efd1137cb2af03b65130ed04
motakashi <takashi-going-sky@hotmail.co.jp>
2025-01-03 17:06:23 +0900

    更新処理の追加

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,8 @@ app.get("/allTodos", async (req: Request, res: Response) => {
 
 app.post("/createTodo", async(req: Request, res: Response) => {
     try {
-        console.log("aaaa")
         console.log(req.body);
         const {title, isCompleted} = req.body;
-        console.log(`title: ${title}, isCompeted: ${isCompleted}`);
         const createTodo = await prisma.todo.create({
             data: {
               title,
@@ -30,9 +28,29 @@ app.post("/createTodo", async(req: Request, res: Response) => {
         console.error(error);
         return res.status(500).json({ error: "An error occurred while creating a todo." });
     }
+});
 
+app.put("/editTodo/:id", async(req: Request, res: Response) =>  {
+    try {
+        const id = Number(req.params.id);
+        console.log(id);
+        console.log(req.body);
+        const {title, isCompleted} = req.body;
+        const editTodo = await prisma.todo.update({
+            where: {id},
+            data: {
+              title,
+              isCompleted
+            },
+          })
+          return res.json(editTodo)
+    } catch (error) {
+        console.error(error);
+        return res.status(400).json(error);
+    }
 });
 
 app.listen(PORT, () => console.log("server is running"));
 console.log(`http://localhost:${PORT}/allTodos`);
 console.log(`http://localhost:${PORT}/createTodo`);
+console.log(`http://localhost:${PORT}/editTodo`);
