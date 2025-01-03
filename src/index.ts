96905ec9cfcbe7601d77d3983f513380bc982d95
motakashi <takashi-going-sky@hotmail.co.jp>
2025-01-03 16:07:45 +0900

    データ取得のメソッドを追加

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import type {Express, Request, Response } from "express";
+import { PrismaClient } from '@prisma/client'
 
 const app: Express = express();
 const PORT = 8080;
 
-app.get("/allTodos", (req: Request, res: Response) => {
-    return res.send("Todo");
+const prisma = new PrismaClient();
+
+app.get("/allTodos", async (req: Request, res: Response) => {
+    const allTodos = await prisma.todo.findMany()
+    return res.json(allTodos);
 })
 
 app.listen(PORT, () => console.log("server is running"));
